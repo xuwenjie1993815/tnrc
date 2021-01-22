7e0a6e9299e664e8088796260631bc07845c79cf
xuwenjie1993815 <380935912@qq.com>
2021-01-22 17:34:01 +0800

    2

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -9,9 +9,11 @@ class Index extends Controller
 {
     public function index()
     {
+    	//获取职位列表
     	$where['deadline'] = ['egt',time()];
     	$where['headhunting'] = '1';
     	$list = Db::table('hr_jobs')->where($where)->select();
+    	//前端需两列显示
     	$arr1 = array();
     	$arr2 = array();
     	foreach ($list as $key => $value) {
@@ -23,8 +25,27 @@ class Index extends Controller
     		}
     	}
     	// var_dump($list);die;
-    	$this->assign('list1',$arr1);
-    	$this->assign('list2',$arr2);
+    	$this->assign('job_list1',$arr1);
+    	$this->assign('job_list2',$arr2);
+
+    	//获取简历列表
+    	$where_resume['headhunting'] = '1';
+    	$list_resume = Db::table('hr_resume')->where($where_resume)->select();
+    	//前端需两列显示
+    	$arr_resume1 = array();
+    	$arr_resume2 = array();
+    	foreach ($list_resume as $key => $value) {
+    		$key_r = is_numeric($key)&($key&1);
+    		if ($key_r == 0) {
+    			$arr_resume1[] = $value;
+    		}else{
+    			$arr_resume2[] = $value;
+    		}
+    	}
+    	// var_dump($list);die;
+    	$this->assign('resume_list1',$arr_resume1);
+    	$this->assign('resume_list2',$arr_resume2);
+
     	return view('Index/Oficiona');
     }
 
